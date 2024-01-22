79e841a0fa5294508e8b27a1b1aeb54a9c35acfc
Kaylee de Soto <kdesoto@g.harvard.edu>
2024-01-22 10:02:45 -0500

    Applied LINCC Frameworks template

---
+++ docs/conf.py
@@ -0,0 +1,48 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+import os
+import sys
+from importlib.metadata import version
+
+# Define path to the code to be documented **relative to where conf.py (this file) is kept**
+sys.path.insert(0, os.path.abspath("../src/"))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "survey-agnostic-sn-vae"
+copyright = "2023, Kaylee de Soto & Ana Sofia Uzsoy"
+author = "Kaylee de Soto & Ana Sofia Uzsoy"
+release = version("survey-agnostic-sn-vae")
+# for example take major/minor
+version = ".".join(release.split(".")[:2])
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.mathjax", "sphinx.ext.napoleon", "sphinx.ext.viewcode"]
+
+extensions.append("autoapi.extension")
+extensions.append("nbsphinx")
+
+templates_path = []
+exclude_patterns = ["_build", "**.ipynb_checkpoints"]
+
+# This assumes that sphinx-build is called from the root directory
+master_doc = "index"
+# Remove 'view source code' from top of page (for html, not python)
+html_show_sourcelink = False
+# Remove namespaces from class/method signatures
+add_module_names = False
+
+autoapi_type = "python"
+autoapi_dirs = ["../src"]
+autoapi_ignore = ["*/__main__.py", "*/_version.py"]
+autoapi_add_toc_tree_entry = False
+autoapi_member_order = "bysource"
+
+html_theme = "sphinx_rtd_theme"

5199680929b7751699f0f5a33a064b49012bfee8
asmuzsoy <amuzsoy@ncsu.edu>
2024-01-22 12:20:32 -0500

    rewrote to use dictionaries

--- src/survey_agnostic_sn_vae/data_generation/objects.py
+++ src/survey_agnostic_sn_vae/data_generation/objects.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class Survey:
     def __init__(self, name, bands, cadence):
@@ -10,31 +10,29 @@ class Survey:
         initial_time = -5 + np.random.random_sample() * 10
         return [initial_time + self.cadence * i for i in range(num_points)]
         
-        
-        
-        
-        
 
         
 class LightCurve:
-    def __init__(self, bands, timepoints, flux, flux_err, survey):
-        self.bands = bands
+    def __init__(self, timepoints, flux, flux_err, survey):
+        self.bands = flux.keys()
         self.timepoints = timepoints
-        
-        if self.flux.shape != (len(self.bands), len(self.timepoints)):
-            raise ValueError('Flux array shape should be (bands, timepoints)')
-            
-        if self.flux_err.shape != self.flux.shape:
-            raise ValueError('Flux and flux err should be the same shape')
-            
+        if flux.keys() !== flux_err.keys():
+            raise ValueError("Make sure flux and flux err have the same bands")
         self.flux = flux
         self.flux_err = flux_err
         self.survey = survey
         
-    def find_max_flux(self, band = None):
-        if band == None:
-            best_indexes = np.argmax(flux, dims=1)
-            return best_indexes
+    def find_max_flux(self, bands = None):
+        if bands == None:
+            bands = self.bands
+        max_times = {}
+        max_fluxes = {}
+        for band in bands:
+            max_index = np.argmax(self.flux[band])
+            max_times[band] = self.timepoints[max_index]
+            max_fluxes[band] = self.flux[band][max_index]
+        return max_times, max_fluxes
+            
         
         
         

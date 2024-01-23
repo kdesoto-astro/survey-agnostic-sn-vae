fa65dfbf3a441a3fd6fba75df413329f384c9b89
Kaylee de Soto <kdesoto@g.harvard.edu>
2024-01-23 15:00:30 -0500

    Preprocessing mostly done, not debugged

--- src/survey_agnostic_sn_vae/preprocessing.py
+++ src/survey_agnostic_sn_vae/preprocessing.py
@@ -1,8 +1,10 @@
 import os, glob
+from survey_agnostic_sn_vae.data_generation.utils import *
+from superraenn.preprocessing import save_lcs
 
 def generate_superraenn_lc_file(
     transient_dir,
-    save_path
+    save_dir
 ):
     """Generate lcs.npz file for use by SuperRAENN.
     """
@@ -13,19 +15,19 @@ def generate_superraenn_lc_file(
         Transient.load(t) for t in all_transient_files
     ]
     
-    # This needs to be redone when retrained
-    # TODO: Need to change this whenever you retrain...
-    filt_dict = {'g': 0, 'r': 1, 'i': 2, 'z': 3}
-    wvs = np.asarray([5460, 6800, 7450, 8700])
-
-    my_lcs = []
+    sr_lcs = []
 
     # Update the LC objects with info from the metatable
     for transient in all_transients:
         for lc in transient.lightcurves:
             params = self.model_params
         
-            t, f, ferr, b = lc.get_arrays()
+            t, m, merr, b = lc.get_arrays()
+            
+            f, ferr = convert_mag_to_flux(
+                m, merr, DEFAULT_ZPT
+            )
+            
             sr_lc = Lightcurve(
                 name=lc.obj_id,
                 times=t,
@@ -36,14 +38,14 @@ def generate_superraenn_lc_file(
             )
             
             sr_lc.add_LC_info(
-                zpt=params['zeropoint'],
+                zpt=DEFAULT_ZPT,
                 mwebv=0.0,
                 redshift=params['redshift'],
-                lim_mag=args.lm,
+                lim_mag=lc.survey.lim_mag,
                 obj_type=obj_types[i]
             )
             
-            sr_lc.wavelengths = lc.survey.wavel
+            sr_lc.wavelengths = lc.survey.band_wavelengths
             sr_lc.get_abs_mags()
             sr_lc.sort_lc()
             pmjd = my_lc.find_peak(
@@ -54,4 +56,6 @@ def generate_superraenn_lc_file(
             sr_lc.filter_names_to_numbers(filt_dict)
             sr_lc.cut_lc()
             sr_lc.make_dense_LC(len(lc.bands))
-            sr_lcs.append(my_lc)
+            sr_lcs.append(sr_lc)
+            
+    save_lcs(sr_lcs, save_dir)
